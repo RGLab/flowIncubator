8c44a10eab6b56900d5ab6988681ab5c2558fac4
mikejiang <wjiang2@fhcrc.org>
2014-07-09 15:27:18 -0700

    change getKeywords into s4 method to avoid masking flowWorkspace method

--- R/getKeywords.R
+++ R/getKeywords.R
@@ -2,10 +2,13 @@
 #' Return them as a data.table with columns names for the keyword
 #' The FIL keyword is renamed to 'name' for sample name consistency
 #' No error checking at the moment.
-getKeywords<-function(gs,kv){
+setMethod("getKeywords",c("GatingSet","character"),function(obj,y){
+      gs <- obj
+      kv <- y
+
   r<-as.data.frame(do.call(cbind,lapply(kv,function(k){
     keyword(gs,k)[1]
   })))
   data.table::setnames(r,"$FIL","name")
   r
-}
+})
