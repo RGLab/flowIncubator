b451fc02c974177453cbb40e58e4e57a27185328
ptvan <pvan@fhcrc.org>
2015-11-05 15:57:36 -0800

    initial commit of runTSNE.R

---
+++ R/runTSNE.R
@@ -0,0 +1,120 @@
+#' function that runs the dimension-reduction algorithm tSNE (t-Distributed Stochastic Neighbor Embedding, Van der Maaten's Barnes-Hut implementation, R pkg 'Rtsne') on a gatingSet
+#' Will sample the minimal number of cells available in all samples to generate balanced cell counts
+#' 
+#' IMPORTANT: Requires a valid gatingSet with cytokine gates downstream of a parent gate
+#' Also expects that pData(gs) contains at least columns: 'name', 'ptid'
+#' 
+#' @param gs a GatingSet object, properly gated data with annotation in its pData
+#' @param parentGate a \code{string} describing the gate upstream of the cytokine gates (eg. "CD4", "cd8+", etc...)
+#' @param cytokine a \code{vector} of \code{strings} describing the cytokine gates immediately downstream of parentGate, eg: "IL2", "IFNg"
+#' @param otherMarkers the remaining markers of the data
+#' @param markerMap named list of marker names to gate names, eg.  list("CD4/IL2" = "IL2","CD4/IFNg" = "IFNg")
+#' @param groupBy columns of the \code{gatingSet}
+#' @param seed a seed since tSNE is random
+#' @param theta parameter to be passed to the \code{Rtsne} function
+#' @param ... other parameters to be passed to the \code{Rtsne} function
+#' @return a \code{matrix} of X and Y coordinates
+#' 
+require(flowWorkspace)
+require(data.table)
+require(Rtsne)
+
+runTSNE <- function(gs, parentGate, cytokines, otherMarkers, markerMap, groupBy, seed=999, theta=0.9 ...){
+  set.seed(seed) 
+  
+  #get pData
+  pd <- as.data.table(pData(gs))
+  
+  #get cell counts of the parent gate
+  print("getting total cell counts from parent gate '", parentGate, "'...")
+  parent_count <- unlist(lapply(gs, function(gh)getTotal(gh, parentGate)))
+  setnames(parent_count,c("name",parentGate))
+  
+  pd <- merge(pd, parent_count,by="name")
+  
+  nTcells <- min(pd[, sum(parentGate), by = groupBy][, V1])
+  print("all samples have at least", nTcells, "cells. This will be the sample size...")
+  #' NOTE: this will fail if one or more sample has no cells !!!
+  
+  pd[, {
+    
+    #subsample entire group together
+    totalEvents <- sum(parent_count)
+    gInd <- 1:totalEvents
+    gInd <- sample.int(totalEvents, size = nTcells)
+    #convert it to logical vector
+    gInd.logical <- rep(F, totalEvents)
+    gInd.logical[gInd] <- T
+    
+    #split it into sample level
+    sn.factor <- unlist(sapply(name, function(sn)rep(sn, .SD[name == sn, parentGate])))
+    ind.vec <- split(gInd.logical, sn.factor)
+    #       
+    #reset indices for each sample
+    for(sn in name)
+    {
+      thisInd <- ind.vec[[sn]]
+      gh <- gs[[sn]]
+      updateIndices(gh, parentGate, thisInd)
+    }
+    
+  }
+  , by = groupBy]
+  print("resampling complete !!!")
+  
+  nodes <- getChildren(gs[[1]], parentGate, path = 2)
+  
+  #' recompute down-stream gates due to the resetting of parent gate indices
+  for(node in nodes)
+    recompute(gs, node)
+  
+  res <- getSingleCellExpression(gs
+                                 , nodes
+                                 , other.markers = otherMarkers
+                                 , map = markerMap
+                                 )
+                                 , threshold = FALSE
+  )
+  
+  res_mask <- getSingleCellExpression(gs
+                                      , nodes
+                                      ,  map = markerMap
+                                      , threshold = T
+  )
+  
+  #' collapse the single Cell expression data matrices across samples
+  res_collapse <- ldply(names(res), function(sn){
+    message(".", appendLF = F)
+    mat <- as.data.frame(res[[sn]])
+    
+    if(nrow(mat) > 0){
+      #compute degree of functionality      
+      mat_mask <- res_mask[[sn]]
+      mat_mask[mat_mask > 0] <- 1
+      mat[, "degree"] <- rowSums(mat_mask)
+      #compute the boolean expression of each event
+      mat[, "poly"] <- Reduce(paste0, as.list(as.data.frame(mat_mask)))
+      pd <- pData(gs[[sn]])
+      rownames(pd) <- NULL
+      cbind(mat, pd)  
+    }else
+      NULL
+  })
+  
+  print("cytokines:", cytokines)
+  print("other markers:", otherMarkers)
+  
+  included_markers <- c(cytokines, otherMarkers)
+  input_mat <- as.matrix(input[,included_markers])
+  
+  print("input is ready, starting tSNE run... \n")
+  system.time(tsne_out <- Rtsne(input_mat, check_duplicates = FALSE, ...))
+  
+  dat <- tsne_out$Y
+  colnames(dat) <- c("x", "y")
+  dat <- cbind(dat, res_collapse)
+  dat <- data.table(dat)
+  print("DONE !")
+  return(dat)
+  
+}
