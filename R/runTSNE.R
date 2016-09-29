6baf9609a89a8be233a4b65e60b7b73326150cec
ptvan <pvan@fhcrc.org>
2016-09-29 15:21:47 -0700

    added parameter  to pass onto getSingleCellExpression()

--- R/runTSNE.R
+++ R/runTSNE.R
@@ -9,6 +9,7 @@
 #' @param cytokine a \code{vector} of \code{strings} describing the cytokine gates immediately downstream of parentGate, eg: "IL2", "IFNg"
 #' @param otherMarkers the remaining markers of the data
 #' @param markerMap named list of marker names to gate names, eg.  list("CD4/IL2" = "IL2","CD4/IFNg" = "IFNg")
+#' @param swap boolean for whether marker and gate names (from markerMap above) should be swapped. Passed onto getSingleCellExpression()
 #' @param groupBy columns of the \code{gatingSet}'s phenoData, same number of cells will be sampled from each group
 #' @param degreeFilter keep cells of this degree and higher, useful when tSNE takes too long to run
 #' @param seed since tSNE is random, need a random seed so we can reproduce results
@@ -16,118 +17,88 @@
 #' @param ... other parameters to be passed to the \code{Rtsne} function
 #' @return a \code{matrix} of X and Y coordinates
 #' 
-runTSNE <- function(gs, parentGate, cytokines, otherMarkers, markerMap, groupBy, degreeFilter = 0,seed=999, theta=0.9, ...){
+runTSNE <- function (gs, parentGate, cytokines, otherMarkers, markerMap, swap,
+                     groupBy, degreeFilter = 0, seed = 999, theta = 0.9, ...) {
+  
   require(flowWorkspace)
   require(flowIncubator)
   require(data.table)
   require(plyr)
   require(Rtsne)
   
-  set.seed(seed) 
-  
-  #get pData
+  set.seed(seed)
   pd <- as.data.table(pData(gs))
-  
-  # keep track of metadata columns (we cut these out of the matrix to be fed to Rtsne)
   meta_cols <- colnames(pd)
   meta_cols <- c(meta_cols, "degree", "poly")
-  
-  #get cell counts of the parent gate
-  cat("getting total cell counts from parent gate", parentGate, "\n")
-  parent_count <- unlist(lapply(gs, function(gh)getTotal(gh, parentGate)))
+  cat("getting total cell counts from parent gate", parentGate, 
+      "\n")
+  parent_count <- unlist(lapply(gs, function(gh) getTotal(gh, 
+                                                          parentGate)))
   parent_count = ldply(parent_count)
-  
-  # col <- eval(paste(parentGate,"count", sep="_"))
-  
-  setnames(parent_count,c("name",parentGate))
-  
-  pd <- merge(pd, parent_count,by="name")
-  
-  nTcells <- min(pd[, sum(get(parentGate)), by = groupBy][, V1])
-  cat("after grouping by '", groupBy, "', all groups will at least", nTcells, "cells." )
-  # NOTE: this will fail if one or more sample has no cells !!!
-  
+  setnames(parent_count, c("name", parentGate))
+  pd <- merge(pd, parent_count, by = "name")
+  nTcells <- min(pd[, sum(get(parentGate)), by = groupBy][, 
+                                                          V1])
+  cat("after grouping by '", groupBy, "', all groups will at least", 
+      nTcells, "cells.\n")
   pd[, {
-    
-    #subsample entire group together
     totalEvents <- sum(get(parentGate))
     gInd <- 1:totalEvents
     gInd <- sample.int(totalEvents, size = nTcells)
-    #convert it to logical vector
     gInd.logical <- rep(F, totalEvents)
     gInd.logical[gInd] <- T
-    
-    #split it into sample level
-    sn.factor <- unlist(sapply(name, function(sn)rep(sn, .SD[name == sn, get(parentGate)])))
+    sn.factor <- unlist(sapply(name, function(sn) rep(sn, 
+                                                      .SD[name == sn, get(parentGate)])))
     ind.vec <- split(gInd.logical, sn.factor)
-    #       
-    #     #reset indices for each sample
-    for(sn in name)
-    {
+    for (sn in name) {
       thisInd <- ind.vec[[sn]]
       gh <- gs[[sn]]
       updateIndices(gh, parentGate, thisInd)
     }
-    
-  }
-  , by = groupBy]
+  }, by = groupBy] 
   cat("subsampling complete ! recomputing... \n")
-  
   nodes <- getChildren(gs[[1]], parentGate, path = 2)
+  for (node in nodes) recompute(gs, node)
+  cat("generating event masks \n")
   
-  #' recompute down-stream gates due to the resetting of parent gate indices
-  for(node in nodes)
-    recompute(gs, node)
+  res <- getSingleCellExpression(gs, nodes, other.markers = otherMarkers, 
+                                 map = markerMap, threshold = FALSE, swap=swap)
   
-  res <- getSingleCellExpression(gs
-                                 , nodes
-                                 , other.markers = otherMarkers
-                                 , map = markerMap
-                                 , threshold = FALSE
-  )
+  res_mask <- getSingleCellExpression(gs, nodes, map = markerMap, 
+                                      threshold = T, swap=swap)
   
-  res_mask <- getSingleCellExpression(gs
-                                      , nodes
-                                      , map = markerMap
-                                      , threshold = T
-  )
-  
-  #' collapse the single Cell expression data matrices across samples
-  res_collapse <- ldply(names(res), function(sn){
+  res_collapse <- ldply(names(res), function(sn) {
     message(".", appendLF = F)
+    # message(sn)
     mat <- as.data.frame(res[[sn]])
-    
-    if(nrow(mat) > 0){
-      #compute degree of functionality      
+    if (nrow(mat) > 0) {
       mat_mask <- res_mask[[sn]]
       mat_mask[mat_mask > 0] <- 1
       mat[, "degree"] <- rowSums(mat_mask)
-      #compute the boolean expression of each event
       mat[, "poly"] <- Reduce(paste0, as.list(as.data.frame(mat_mask)))
       pd <- pData(gs[[sn]])
       rownames(pd) <- NULL
-      cbind(mat, pd)  
-    }else
-      NULL
+      cbind(mat, pd)
+    } 
+    else NULL
   })
+  
   
   cat("\n cytokines:", cytokines)
   cat("\n other markers:", otherMarkers)
   cat("\n input matrix has", nrow(res_collapse), "rows...")
-  
   res_collapse <- subset(res_collapse, degree > degreeFilter)
-  cat("\n input matrix has", nrow(res_collapse), "rows after filtering for cells of degree >", degreeFilter)
-  
-  input_mat <- as.matrix(res_collapse[,!names(res_collapse) %in% meta_cols])
-  
+  cat("\n input matrix has", nrow(res_collapse), "rows after filtering for cells of degree >", 
+      degreeFilter)
+  input_mat <- as.matrix(res_collapse[, !names(res_collapse) %in% 
+                                        meta_cols])
   cat("\n starting tSNE run at ", date(), "\n")
-  system.time(tsne_out <- Rtsne(input_mat, check_duplicates = FALSE, ...))
-  
+  system.time(tsne_out <- Rtsne(input_mat, check_duplicates = FALSE, 
+                                ...))
   dat <- tsne_out$Y
   colnames(dat) <- c("x", "y")
   dat <- cbind(dat, res_collapse)
   dat <- data.table(dat)
-  
   cat("completed tSNE run at", date(), "!\n")
   return(dat)
   
